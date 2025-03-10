5df4568fe8efaab48fa228c021ca9460819e18ae
Mikhail Alexandrov <mikesperanskii@gmail.com>
2025-03-10 17:38:36 +0300

    init and finish

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import BlackSquare from "./components/BlackSquare.vue";
+</script>
+
+<template>
+    <BlackSquare/>
+</template>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/BlackSquare.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="center">
+        <div class="square" />
+    </div>
+</template>
+
+
+
+<style scoped>
+.center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+.square {
+    width: 600px;
+    height: 600px;
+    background-color: black;
+}
+</style>
